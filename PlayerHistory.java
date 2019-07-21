950606aba7c2514b2469d57529288b249bc881a1
Kenton Van <kdvjr@hotmail.com>
2019-07-21 18:16:27 -0400

    Filled out body of class a little bit

--- PlayerHistory.java
+++ PlayerHistory.java
@@ -1,8 +1,33 @@
+import java.util.ArrayList;
+
 /**
  * Contains the results of the past MLB games played by the player
+ * 
  * @author Colin McLaughlin & Kenton Van
  *
  */
 public class PlayerHistory {
 
+	// Variables
+	private ArrayList<Game> games; // Holds the MLB games the player has played
+
+	// Constructor
+	/**
+	 * Constructor for PlayerHistory
+	 * 
+	 * @param games The array of games the player has played in.
+	 */
+	PlayerHistory(ArrayList<Game> games) {
+		this.games = games;
+	}
+
+	// Methods
+	/**
+	 * Getter for games
+	 * 
+	 * @return The array of games the player has played in.
+	 */
+	public ArrayList<Game> getGames() {
+		return this.games;
+	}
 }
