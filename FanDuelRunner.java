2a1fc90a4cacda49e03b0146b6be8726cd56af5c
Kenton Van <kdvjr@hotmail.com>
2019-07-21 18:46:31 -0400

    Initial commit of main executable

---
+++ FanDuelRunner.java
@@ -0,0 +1,24 @@
+/**
+ * Main executable for the FanDuel suggestion program.
+ * 
+ * @author Colin McLaughlin & Kenton Van
+ *
+ */
+public class FanDuelRunner {
+
+	public static void main(String[] args) {
+		// TODO Check if the database has been initialized.  Create if not.
+		
+		// TODO Open the GUI
+		
+		// TODO Convert input spreadsheet into a roster of players
+		
+		// TODO Retrieve historical player information from the database
+		
+		// TODO Calculate best roster suggestion(s)
+		
+		// TODO Display roster results to user
+
+	}
+
+}
