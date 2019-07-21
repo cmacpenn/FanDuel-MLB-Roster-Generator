d3043bb749fdce69a1241ba439e6477598fc186e
Kenton Van <kdvjr@hotmail.com>
2019-07-21 19:03:55 -0400

    Initial commit of GUI

---
+++ FanDuelGUI.java
@@ -0,0 +1,24 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+/**
+ * The GUI for the FanDuel suggestion program
+ * 
+ * @author Colin McLaughlin & Kenton Van
+ *
+ */
+public class FanDuelGUI {
+
+	// Variables
+	private JFrame window; // The main window/frame
+	private JButton uploadPlayerFile; // Uploads the player file from FanDuel to the program.
+
+	// Constructor
+	public FanDuelGUI() {
+		// TODO Initialize GUI
+	}
+
+	// Methods
+	// TODO
+
+}
