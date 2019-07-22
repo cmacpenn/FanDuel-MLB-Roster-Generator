5c0cd94a0d7f8950d4e88a9b6049a40ba15fc61d
Kenton Van <kdvjr@hotmail.com>
2019-07-21 20:02:48 -0400

    Initial commit of Roster

---
+++ Roster.java
@@ -0,0 +1,32 @@
+/**
+ * Holds 9 MLB players with 1 pitcher, 1 center/1st base, 1 2nd base, 1 3rd
+ * base, 1 short stop, 3 out fielders, and 1 utility
+ * 
+ * @author Colin McLaughlin & Kenton Van
+ *
+ */
+public class Roster {
+
+	// Variables
+	Player pitcher;
+	Player center;
+	Player secondBase;
+	Player thirdBase;
+	Player shortstop;
+	Player outfielder1;
+	Player outfielder2;
+	Player outfielder3;
+	Player utility;
+
+	// Constructor
+	/**
+	 * Constructor for Roster
+	 * @param players The list of 9 players.
+	 */
+	public Roster(Player[] players) {
+		// TODO Check that we have 9 players
+		// TODO Check that we have the right types of players
+		// TODO Assign players to their respective variables
+	}
+
+}
