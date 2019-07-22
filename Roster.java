ea87a38e0d7d7cf6b42aeec76a6f137da18d980a
Kenton Van <kdvjr@hotmail.com>
2019-07-22 18:16:20 -0400

    Edited to implement iterable

--- Roster.java
+++ Roster.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
 /**
  * Holds 9 MLB players with 1 pitcher, 1 center/1st base, 1 2nd base, 1 3rd
  * base, 1 short stop, 3 out fielders, and 1 utility
@@ -5,28 +8,36 @@
  * @author Colin McLaughlin & Kenton Van
  *
  */
-public class Roster {
+public class Roster implements Iterable<Player> {
 
 	// Variables
-	Player pitcher;
-	Player center;
-	Player secondBase;
-	Player thirdBase;
-	Player shortstop;
-	Player outfielder1;
-	Player outfielder2;
-	Player outfielder3;
-	Player utility;
+	private ArrayList<Player> players;
 
 	// Constructor
 	/**
 	 * Constructor for Roster
+	 * 
 	 * @param players The list of 9 players.
 	 */
-	public Roster(Player[] players) {
+	public Roster(ArrayList<Player> players) {
 		// TODO Check that we have 9 players
 		// TODO Check that we have the right types of players
-		// TODO Assign players to their respective variables
+		this.players = players;
 	}
+
+	@Override
+	/**
+	 * Implement iterator so we can use for each loops on rosters
+	 */
+	public Iterator<Player> iterator() {
+		Iterator<Player> playerIterator = players.iterator();
+		return playerIterator;
+	}
+
+	// Methods
+
+	// TODO add getter
+
+	// TODO Add methods to return a specific position?
 
 }
