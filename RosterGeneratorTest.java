929a17968970ca40a9eb9b8be7b483e387188f8e
Kenton Van <kdvjr@hotmail.com>
2019-07-22 18:13:40 -0400

    Modified tests to work with Roster class

--- RosterGeneratorTest.java
+++ RosterGeneratorTest.java
@@ -7,23 +7,20 @@ import org.junit.jupiter.api.Test;
 class RosterGeneratorTest {
 
 	/**
-	 * This test tests that the method returns true for 
-	 * gross salary under 35k.
+	 * This test tests that the method returns true for gross salary under 35k.
 	 */
 	@Test
 	void testIsRosterUnderSalaryCap1() {
-		Player one = new Player(1000);
-		Player two = new Player(1000);
-		Player three = new Player(1000);
-		Player four = new Player(1000);
-		Player five = new Player(1000);
-		Player six = new Player(1000);
-		Player seven = new Player(1000);
-		Player eight = new Player(1000);
-		Player nine = new Player(1000);
-		
-		
-		ArrayList<Player> players = new ArrayList<>();
+		Player one = new Player("Bob", 1000, PlayerPosition.PITCHER, null);
+		Player two = new Player("James", 1000, PlayerPosition.CENTERORFIRSTBASE, null);
+		Player three = new Player("Kurt", 1000, PlayerPosition.SECONDBASE, null);
+		Player four = new Player("Bill", 1000, PlayerPosition.THIRDBASE, null);
+		Player five = new Player("Eric", 1000, PlayerPosition.SHORTSTOP, null);
+		Player six = new Player("Sam", 1000, PlayerPosition.OUTFIELD, null);
+		Player seven = new Player("James", 1000, PlayerPosition.OUTFIELD, null);
+		Player eight = new Player("Richard", 1000, PlayerPosition.OUTFIELD, null);
+		Player nine = new Player("Carter", 1000, PlayerPosition.UTILITY, null);
+		ArrayList<Player> players = new ArrayList<Player>();
 		players.add(one);
 		players.add(two);
 		players.add(three);
@@ -33,31 +30,29 @@ class RosterGeneratorTest {
 		players.add(seven);
 		players.add(eight);
 		players.add(nine);
-		
-		Salary s1 = new Salary();
-		
+		Roster roster = new Roster(players);
+
 		boolean expected = true;
-		assertEquals(expected, s1.isRosterUnderSalaryCap(players));
-		
-		
+		boolean actual = RosterGenerator.isRosterUnderSalaryCap(roster);
+		assertEquals(expected, actual);
+
 	}
+
 	/**
-	 * This test tests that gross salary returns false
-	 * when over 35k.
+	 * This test tests that gross salary returns false when over 35k.
 	 */
 	@Test
 	void testIsRosterUnderSalaryCap2() {
-		Player one = new Player(100000);
-		Player two = new Player(1000);
-		Player three = new Player(1000);
-		Player four = new Player(1000);
-		Player five = new Player(1000);
-		Player six = new Player(1000);
-		Player seven = new Player(1000);
-		Player eight = new Player(1000);
-		Player nine = new Player(1000);
-		
-		
+		Player one = new Player("Bob", 100000, PlayerPosition.PITCHER, null);
+		Player two = new Player("James", 1000, PlayerPosition.CENTERORFIRSTBASE, null);
+		Player three = new Player("Kurt", 1000, PlayerPosition.SECONDBASE, null);
+		Player four = new Player("Bill", 1000, PlayerPosition.THIRDBASE, null);
+		Player five = new Player("Eric", 1000, PlayerPosition.SHORTSTOP, null);
+		Player six = new Player("Sam", 1000, PlayerPosition.OUTFIELD, null);
+		Player seven = new Player("James", 1000, PlayerPosition.OUTFIELD, null);
+		Player eight = new Player("Richard", 1000, PlayerPosition.OUTFIELD, null);
+		Player nine = new Player("Carter", 1000, PlayerPosition.UTILITY, null);
+
 		ArrayList<Player> players = new ArrayList<>();
 		players.add(one);
 		players.add(two);
@@ -68,13 +63,12 @@ class RosterGeneratorTest {
 		players.add(seven);
 		players.add(eight);
 		players.add(nine);
-		
-		Salary s1 = new Salary();
-		
+		Roster roster = new Roster(players);
+
 		boolean expected = false;
-		assertEquals(expected, s1.isRosterUnderSalaryCap(players));
-		
-		
+		boolean actual = RosterGenerator.isRosterUnderSalaryCap(roster);
+		assertEquals(expected, actual);
+
 	}
 
 }
