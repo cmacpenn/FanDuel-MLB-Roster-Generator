be85cb6a6e0082a191f7ec8b988e8690850c4c0e
Kenton Van <kdvjr@hotmail.com>
2019-07-28 11:27:30 -0400

    Added code to load database

--- src/FanDuelRunner.java
+++ src/FanDuelRunner.java
@@ -4,15 +4,34 @@
  * @author Colin McLaughlin & Kenton Van
  *
  */
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+
 import javax.swing.SwingUtilities;
 
 public class FanDuelRunner {
 
 	public static void main(String[] args) {
-		// TODO Check if the database has been initialized.  Create if not.
-		
-		// TODO Open the GUI
+		// Open the GUI
 		SwingUtilities.invokeLater(new FanDuelGUI());
+		
+		// Load the database
+		// Does this need to go inside the GUI somehow?
+		String url = "jdbc:h2:mem:";
+		File rawBatterGameData = new File("database/database_batting_game_logs_after_2015.csv");
+		File rawPitcherGameData = new File("database/database_pitching_game_logs_after_2015.csv");
+		File rawPlayerData = new File("database/database_players.csv");
+		try {
+			MLBDatabase mlbDb = new MLBDatabase(url, rawBatterGameData, rawPitcherGameData, rawPlayerData);
+		} catch (SQLException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		
 		// TODO Convert input spreadsheet into a roster of players
 		
 		// TODO Retrieve historical player information from the database
