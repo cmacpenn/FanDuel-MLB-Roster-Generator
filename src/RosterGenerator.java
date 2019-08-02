b8e5262ebcb65da68c30ade5335a4ebccf84f9d4
Kenton Van <kdvjr@hotmail.com>
2019-08-01 20:42:22 -0400

    fixed trailing bracket

--- src/RosterGenerator.java
+++ src/RosterGenerator.java
@@ -13,13 +13,13 @@ public class RosterGenerator {
 	// Variables
 	private ArrayList<Player> players; // The list of MLB players to choose from
 	private static final int SALARYCAP = 35000; // The salary cap for a team, in dollars
-	private ArrayList<Player> pitchers; 
-	private ArrayList<Player> catcherOrFirsts; 
+	private ArrayList<Player> pitchers;
+	private ArrayList<Player> catcherOrFirsts;
 	private ArrayList<Player> seconds;
 	private ArrayList<Player> thirds;
 	private ArrayList<Player> shorts;
 	private ArrayList<Player> outfields;
-	
+
 	// Constructor
 	/**
 	 * Constructor for RosterGenerator
@@ -53,9 +53,9 @@ public class RosterGenerator {
 			Roster emptyRoster = new Roster(noPlayer);
 			return emptyRoster;
 		} else {
-		ArrayList<Roster> manyRosters = this.generateMultipleRandomRosters(10000);
-		Roster recommendedRoster = this.rankRosters(manyRosters);
-		return recommendedRoster;
+			ArrayList<Roster> manyRosters = this.generateMultipleRandomRosters(10000);
+			Roster recommendedRoster = this.rankRosters(manyRosters);
+			return recommendedRoster;
 		}
 	}
 
@@ -94,8 +94,7 @@ public class RosterGenerator {
 	public Roster chooseRandomRoster() {
 		ArrayList<Player> team = new ArrayList<Player>();
 		ArrayList<Player> teamNoWild = new ArrayList<Player>();
-		if (this.checkPlayers(players, pitchers, seconds, thirds, shorts, 
-				catcherOrFirsts, outfields)) {
+		if (this.checkPlayers(players, pitchers, seconds, thirds, shorts, catcherOrFirsts, outfields)) {
 			Player pitcher = this.getRandomPlayer(this.pitchers);
 			Player catcherOrFirst = this.getRandomPlayer(this.catcherOrFirsts);
 			Player second = this.getRandomPlayer(this.seconds);
@@ -116,16 +115,14 @@ public class RosterGenerator {
 			team = teamNoWild;
 			team.add(pitcher);
 			team.add(wild);
-			
+
 			Roster roster = new Roster(team);
-			return roster;	
-			}
+			return roster;
+		}
 		Roster notEnoughPlayers = new Roster(team);
 		System.out.println("Error: Not enough players");
 		return notEnoughPlayers;
-			
-		}	
-		
+
 	}
 
 	/**
@@ -135,13 +132,14 @@ public class RosterGenerator {
 	 * @return True, if the roster has a combined salary under the SALARYCAP. False
 	 *         otherwise.
 	 */
-	//TODO Revert to private non-static when tests are done.  Temporary fix for testing deliverable.
+	// TODO Revert to private non-static when tests are done. Temporary fix for
+	// testing deliverable.
 	public static boolean isRosterUnderSalaryCap(Roster roster) {
 		int totalSalary = 0;
-		for (Player player : roster){
+		for (Player player : roster) {
 			totalSalary += player.getSalary();
 		}
-		if (totalSalary <= SALARYCAP){
+		if (totalSalary <= SALARYCAP) {
 			return true;
 		} else {
 			return false;
@@ -158,11 +156,12 @@ public class RosterGenerator {
 	public Roster rankRosters(ArrayList<Roster> rosters) {
 		ArrayList<Roster> sortedRoster = rosters;
 		Collections.sort(sortedRoster);
-		return sortedRoster.get(sortedRoster.size()-1);
+		return sortedRoster.get(sortedRoster.size() - 1);
 	}
 
 	/**
 	 * Get a random player
+	 * 
 	 * @param list
 	 * @return
 	 */
@@ -170,9 +169,10 @@ public class RosterGenerator {
 		Collections.shuffle(list);
 		return list.get(0);
 	}
-	
+
 	/**
 	 * Return 3 outfielders that are not duplicates
+	 * 
 	 * @param list
 	 * @return
 	 */
@@ -188,8 +188,10 @@ public class RosterGenerator {
 		}
 		return nonDuplicate;
 	}
+
 	/**
 	 * Return a random player of ones not already used.
+	 * 
 	 * @param list
 	 * @return
 	 */
@@ -199,32 +201,37 @@ public class RosterGenerator {
 		Iterator<Player> iterator = allPlayers.iterator();
 		String pitch = "PITCHER";
 		while (iterator.hasNext()) {
-			if(pitch.contentEquals(iterator.next().getPosition().name())) {
+			if (pitch.contentEquals(iterator.next().getPosition().name())) {
 				iterator.remove();
-			} 
-			
+			}
+
 		}
 		allPlayers.removeAll(list);
-		
+
 		Player wild = this.getRandomPlayer(allPlayers);
 		return wild;
 	}
+
 	/**
 	 * Return an array of specific positions
+	 * 
 	 * @param position
 	 * @return
 	 */
-	private ArrayList<Player> generatePositionArray(String position){
+	private ArrayList<Player> generatePositionArray(String position) {
 		ArrayList<Player> generate = new ArrayList<Player>();
 		for (Player player : this.players) {
 			if (position.contentEquals(player.getPosition().name())) {
 				generate.add(player);
-				
-			}		
-		} return generate;
+
+			}
+		}
+		return generate;
 	}
+
 	/**
 	 * Check that the array of players have enough for one team
+	 * 
 	 * @param p
 	 * @param p1
 	 * @param p2
@@ -234,18 +241,14 @@ public class RosterGenerator {
 	 * @param p6
 	 * @return
 	 */
-	private boolean checkPlayers(ArrayList<Player> p, ArrayList<Player> p1,
-			ArrayList<Player> p2, ArrayList<Player> p3, ArrayList<Player> p4,
-			ArrayList<Player> p5, ArrayList<Player> p6) {
-		if (p.size() >= 9 && p1.size() >= 1 && p2.size() >= 1 
-				&& p3.size() >= 1 && p4.size() >=1 && 
-				p5.size() >= 1 && p6.size() >= 3) {
+	private boolean checkPlayers(ArrayList<Player> p, ArrayList<Player> p1, ArrayList<Player> p2, ArrayList<Player> p3,
+			ArrayList<Player> p4, ArrayList<Player> p5, ArrayList<Player> p6) {
+		if (p.size() >= 9 && p1.size() >= 1 && p2.size() >= 1 && p3.size() >= 1 && p4.size() >= 1 && p5.size() >= 1
+				&& p6.size() >= 3) {
 			return true;
 		} else {
 			return false;
 		}
 	}
-	
-	
 
 }
